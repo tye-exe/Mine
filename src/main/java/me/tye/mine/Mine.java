b4c351fe6f3dcd5bbafdd29df85a069dddb1fae6
Mapty231 <tye.exe@gmail.com>
2023-11-18 19:42:21 +0000

    Init

---
+++ src/main/java/me/tye/mine/Mine.java
@@ -0,0 +1,20 @@
+package me.tye.mine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class Mine extends JavaPlugin {
+
+@Override
+public void onEnable() {
+    // Plugin startup logic
+
+
+}
+
+@Override
+public void onDisable() {
+    // Plugin shutdown logic
+}
+}
