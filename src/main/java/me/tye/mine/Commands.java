46e4d91a98b373c644fbbccc49d1d5881a36e2aa
Mapty231 <tye.exe@gmail.com>
2023-11-19 17:40:09 +0000

    Renamed "/mine selector" to "/mine pointer"

--- src/main/java/me/tye/mine/Commands.java
+++ src/main/java/me/tye/mine/Commands.java
@@ -1,6 +1,5 @@
 package me.tye.mine;
 
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,29 +8,26 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import static me.tye.mine.Util.itemProperties;
-import static me.tye.mine.Util.selector;
 
 public class Commands implements CommandExecutor {
 @Override
 public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-  if (args.length < 1)
-    return true;
+  if (args.length < 1) return true;
 
-  if (args[0].equals("select")) {
-    giveSelector(sender);
+  if (args[0].equals("pointer")) {
+    givePointer(sender);
   }
 
   return true;
 }
 
-private void giveSelector(CommandSender sender) {
+private void givePointer(CommandSender sender) {
   if (sender instanceof Player player) {
 
-    ItemStack selector = itemProperties(Util.selector, "Selector", "selector");
-    if (Util.inventoryContainsIdent(player.getInventory(), "selector"))
-      return;
+    ItemStack pointer = itemProperties(Util.pointer, "Pointer", "pointer");
+    if (Util.inventoryContainsIdententity(player.getInventory(), "pointer")) return;
 
-    player.getInventory().addItem(selector);
+    player.getInventory().addItem(pointer);
   }
 }
 
