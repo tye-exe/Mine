196d1e473da380e90b7a5fa8b3c35a72db86606f
Mapty231 <tye.exe@gmail.com>
2023-11-23 11:59:53 +0000

    updated key enum

--- src/main/java/me/tye/mine/utils/Key.java
+++ src/main/java/me/tye/mine/utils/Key.java
@@ -2,16 +2,10 @@ package me.tye.mine.utils;
 
 public enum Key {
 
-  key("key"),
-  player("player"),
-  filePath("filePath");
+  key(),
+  filePath();
 
-private final String valueToReplace;
 private String replaceWith = "";
-
-Key(String valueToReplace) {
-  this.valueToReplace = valueToReplace;
-}
 
 
 /**
@@ -28,13 +22,5 @@ public Key replaceWith(String string) {
  */
 public String getReplaceWith() {
   return replaceWith;
-}
-
-/**
- * @return The value to replace.
- */
-@Override
-public String toString() {
-  return this.valueToReplace;
 }
 }
