abeda822679821aa1104dbbd26591e7cd66ce731
Mapty231 <tye.exe@gmail.com>
2023-11-25 08:22:59 +0000

    Finished selection outline rendering.

--- src/main/java/me/tye/mine/PlayerClick.java
+++ src/main/java/me/tye/mine/PlayerClick.java
@@ -1,10 +1,7 @@
 package me.tye.mine;
 
 import me.tye.mine.utils.Identifier;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,9 +9,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
-
-import static me.tye.mine.utils.Util.getSurrounding;
-import static me.tye.mine.utils.Util.plugin;
 
 public class PlayerClick implements Listener {
 
@@ -41,32 +35,12 @@ private static void select(@NotNull PlayerInteractEvent e) {
   if (clickedBlock == null) return;
 
 
-  Selection selected = new Selection(player.getUniqueId());
+  Selection selected = new Selection(player);
   if (Selection.selections.containsKey(player.getUniqueId())) {
     selected = Selection.selections.get(player.getUniqueId());
   }
 
   Selection.selections.put(player.getUniqueId(), selected.setLocation(clickedBlock.getLocation(), action));
-
-
-  //if the block is the same then use secondary
-  BlockData cornerBlock = Material.MAGENTA_GLAZED_TERRACOTTA.createBlockData();
-
-  if (action.isRightClick()) {
-    cornerBlock = Material.ORANGE_GLAZED_TERRACOTTA.createBlockData();
-  }
-
-  //sendBlockChange needs to be in a runnable as the server sends a block update packet to the client after processing this event.
-  final BlockData changedBlock = cornerBlock;
-  Bukkit.getScheduler().runTaskLater(plugin, () -> {
-    player.sendBlockChange(clickedBlock.getLocation(), changedBlock);
-
-    //makes the selected block glow.
-    for (Block block : getSurrounding(clickedBlock, Material.AIR)) {
-      player.sendBlockChange(block.getLocation(), Material.LIGHT.createBlockData());
-    }
-
-  }, 1);
 
 }
 
--- src/main/java/me/tye/mine/Selection.java
+++ src/main/java/me/tye/mine/Selection.java
@@ -1,47 +1,130 @@
 package me.tye.mine;
 
+import me.tye.mine.utils.SendBlockChanges;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
 import org.bukkit.Location;
-import org.bukkit.Particle;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
-import static me.tye.mine.utils.Util.getSurrounding;
+import static me.tye.mine.utils.Util.*;
 
 public class Selection {
 
 public static final HashMap<UUID, Selection> selections = new HashMap<>();
 private final UUID playerID;
+private final Player player;
 
 private Location startLoc = new Location(Bukkit.getWorlds().get(0), 0, Double.MAX_VALUE, 0);
 private Location endLoc = new Location(Bukkit.getWorlds().get(0), 0, Double.MAX_VALUE, 0);
 
 private final ArrayList<Location> selected = new ArrayList<>();
 
+/**
+ Stores the current outline of the selection that is shown to the player. The specific selected blocks are not included in this.
+ */
+private final LinkedHashSet<Location> selectionOutline = new LinkedHashSet<>();
 
-public Selection(@NotNull UUID playerID) {
-  this.playerID = playerID;
 
-  renderOutline();
+//boiler plate start
+public Selection(@NotNull Player player) {
+  this.player = player;
+  this.playerID = player.getUniqueId();
+}
+
+
+public @NotNull Location getEndLoc() {
+  return endLoc;
+}
+
+public @NotNull Location getStartLoc() {
+  return startLoc;
 }
 
 /**
- Moves the end location to a list of selected positions. And sets the given location to be the end location.
- * @param selectedLocation The given location.
+ * @return True if the user has set an end location.
  */
-public void add(@NotNull Location selectedLocation) {
-  selected.add(endLoc);
-  setEndLoc(selectedLocation);
+public boolean hasSetEndLocation() {
+  return getEndLoc().getY() != Double.MAX_VALUE;
 }
+
+/**
+ * @return True if the user has set a start location.
+ */
+public boolean hasSetStartLocation() {
+  return getStartLoc().getY() != Double.MAX_VALUE;
+}
+
+/**
+ * @return True if the user has a start location selected & an end location selected.
+ */
+public boolean hasSelection() {
+  return hasSetEndLocation() && hasSetStartLocation();
+}
+
+//boiler plate end
+
+
+/**
+ Sets the start location that the player selected & sends a fake block packet to the player, so that they can see the block they've selected.<br>
+ If the player has made two or more selections then the outline of the selected area is rendered client-side.
+ * @param startLoc The start location selected.
+ */
+public void setStartLoc(@NotNull Location startLoc) {
+  restoreBlocks(getStartLoc(), false);
+
+  Location oldLocation = getStartLoc();
+  this.startLoc = startLoc;
+
+  //If the player has made a selection of two or more blocks then the selection is rendered.
+  if (hasSelection()) {
+    renderSelection(oldLocation, getStartLoc(), firstSelectedBlock);
+    return;
+  }
+
+  Collection<BlockState> cornerStates = getNewCornerSurroundingStates(getStartLoc());
+
+  BlockState state = getStartLoc().getBlock().getState();
+  state.setType(firstSelectedBlock);
+  cornerStates.add(state);
+
+  //Sends the new update blocks a tick later, since when the player clicks on a block, a sends a block update packet.
+  Bukkit.getScheduler().runTaskLater(plugin, new SendBlockChanges().init(cornerStates, player), 1);
+}
+
+/**
+ Sets the end location that the player selected & sends a fake block packet to the player, so that they can see the block they've selected.<br>
+ If the player has made two or more selections then the outline of the selected area is rendered client-side.
+ * @param endLoc The end location selected.
+ */
+public void setEndLoc(@NotNull Location endLoc) {
+  restoreBlocks(getEndLoc(), false);
+
+  Location oldLocation = getEndLoc();
+  this.endLoc = endLoc;
+
+  //If the player has made a selection of two or more blocks then the selection is rendered.
+  if (hasSelection()) {
+    renderSelection(oldLocation, getEndLoc(), lastSelectedBlock);
+    return;
+  }
+
+  Collection<BlockState> cornerStates = getNewCornerSurroundingStates(getEndLoc());
+
+  BlockState state = getEndLoc().getBlock().getState();
+  state.setType(lastSelectedBlock);
+  cornerStates.add(state);
+
+  //Sends the new update blocks a tick later, since when the player clicks on a block, a sends a block update packet.
+  Bukkit.getScheduler().runTaskLater(plugin, new SendBlockChanges().init(cornerStates, player), 1);
+}
+
 
 /**
  Sets the start or end location to the given location based on the given action.<br>
@@ -82,56 +165,32 @@ public boolean hasSetLocation(@NotNull Action action) {
   return false;
 }
 
-/**
- * @return True if the user has set an end location.
- */
-public boolean hasSetEndLocation() {
-  return endLoc.getY() != Double.MAX_VALUE;
-}
-
-/**
- * @return True if the user has set a start location.
- */
-public boolean hasSetStartLocation() {
-  return endLoc.getY() != Double.MAX_VALUE;
-}
-
-/**
- * @return True if the user has a start location selected & an end location selected.
- */
-public boolean hasSelection() {
-  return hasSetEndLocation() && hasSetStartLocation();
-}
 
 /**
  Changes all the blocks selected by a player to the server-side state.
  */
 public void restore() {
+  //Restores the blocks that a player has selected.
   restoreBlocks(getStartLoc(), true);
   restoreBlocks(getEndLoc(), true);
 
   for (Location restoreLocation : selected) {
     restoreBlocks(restoreLocation, true);
   }
+
+  //Restores the outline of the selection area.
+  if (player == null) return;
+  if (selectionOutline.isEmpty()) return;
+
+  Collection<BlockState> refreshBlocks = new ArrayList<>();
+
+  selectionOutline.forEach(location -> {
+    refreshBlocks.add(location.getBlock().getState());
+  });
+
+  player.sendBlockChanges(refreshBlocks);
 }
 
-public @NotNull Location getEndLoc() {
-  return endLoc;
-}
-
-public @NotNull Location getStartLoc() {
-  return startLoc;
-}
-
-public void setStartLoc(@NotNull Location startLoc) {
-  restoreBlocks(this.startLoc, false);
-  this.startLoc = startLoc;
-}
-
-public void setEndLoc(@NotNull Location endLoc) {
-  restoreBlocks(this.endLoc, false);
-  this.endLoc = endLoc;
-}
 
 /**
  Resends the block data of a three by three area centered on the given location to the client.
@@ -140,9 +199,6 @@ public void setEndLoc(@NotNull Location endLoc) {
  *                          If set to false then adjacent selection blocks won't be resent.
  */
 private void restoreBlocks(@NotNull Location locationToRestore, boolean includeAdjacentSelected) {
-  Player player = Bukkit.getPlayer(playerID);
-  if (player == null) return;
-
   Collection<BlockState> restoreBlocks = new ArrayList<>();
 
   for (Block block : getSurrounding(locationToRestore.getBlock(), null)) {
@@ -161,26 +217,138 @@ private void restoreBlocks(@NotNull Location locationToRestore, boolean includeA
   player.sendBlockChanges(restoreBlocks);
 }
 
-private void renderOutline() {
-  new Thread(() -> {
 
-    //Only runs while the player is selecting.
-    while (selections.containsKey(playerID)) {
-      try {
-        Thread.sleep(250);
-      } catch (InterruptedException ignore) {}
 
-      if (!hasSelection()) continue;
+/**
+ Sends packets to the player restoring the old selected area & the corner to restore to the server-side state.<br>
+ Then sends fake block packets to the player for the new selection outline & the new selected corner.<br>
+ * @param cornerToReRestore The location of the old selected corner that should be restored to a server-side state.
+ * @param newSelectedCorner The location of the new selected corner to change client-side.
+ * @param newSelectedCornerMaterial The material to change the new selected corner to.
+ */
+private void renderSelection(Location cornerToReRestore, Location newSelectedCorner, Material newSelectedCornerMaterial) {
+  //Removes the old outline & corner for the player
+  Collection<BlockState> refreshBlocks = new ArrayList<>();
 
-      Player player = Bukkit.getPlayer(playerID);
+  selectionOutline.forEach(location -> {
+    refreshBlocks.add(location.getBlock().getState());
+  });
 
-      if (player == null) return;
+  refreshBlocks.add(cornerToReRestore.getBlock().getState());
 
-      Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(255, 0, 0), 1.0F);
-      player.spawnParticle(Particle.REDSTONE, player.getLocation().add(0.0, 2.0, 0.0), 5, 0.0, 1.0, 0.0, dustOptions);
+  player.sendBlockChanges(refreshBlocks);
 
+
+  selectionOutline.clear();
+
+  World world = player.getWorld();
+
+  int startX = getEndLoc().getBlockX();
+  int startY = getEndLoc().getBlockY();
+  int startZ = getEndLoc().getBlockZ();
+
+  int endX = getStartLoc().getBlockX();
+  int endY = getStartLoc().getBlockY();
+  int endZ = getStartLoc().getBlockZ();
+
+
+  //adds the outline for the X blocks
+  getBetween(startX, endX).forEach((X) -> {
+    selectionOutline.add(new Location(world, X, startY, startZ));
+    selectionOutline.add(new Location(world, X, endY, endZ));
+    selectionOutline.add(new Location(world, X, startY, endZ));
+    selectionOutline.add(new Location(world, X, endY, startZ));
+  });
+
+  //adds the outline for the Y blocks
+  getBetween(startY, endY).forEach((Y) -> {
+    selectionOutline.add(new Location(world, startX, Y, startZ));
+    selectionOutline.add(new Location(world, endX, Y, endZ));
+    selectionOutline.add(new Location(world, startX, Y, endZ));
+    selectionOutline.add(new Location(world, endX, Y, startZ));
+  });
+
+  //adds the outline for the Z blocks
+  getBetween(startZ, endZ).forEach((Z) -> {
+    selectionOutline.add(new Location(world, startX, startY, Z));
+    selectionOutline.add(new Location(world, endX, endY, Z));
+    selectionOutline.add(new Location(world, startX, endY, Z));
+    selectionOutline.add(new Location(world, endX, startY, Z));
+  });
+
+
+  //removes the selection blocks if they got added.
+  selectionOutline.remove(getStartLoc());
+  selectionOutline.remove(getEndLoc());
+
+
+  //Sends the new outline & corner to the player
+  Collection<BlockState> updateBlocks = new ArrayList<>();
+
+  selectionOutline.forEach(location -> {
+    BlockState state = location.getBlock().getState();
+    state.setType(Material.RED_STAINED_GLASS);
+    updateBlocks.add(state);
+  });
+
+  //Adds the new corner
+  BlockState cornerState = newSelectedCorner.getBlock().getState();
+  cornerState.setType(newSelectedCornerMaterial);
+  updateBlocks.add(cornerState);
+
+  //Makes the new corner glow
+  updateBlocks.addAll(getNewCornerSurroundingStates(newSelectedCorner));
+
+  //Sends the new update blocks a tick later, since when the player clicks on a block, a sends a block update packet.
+  Bukkit.getScheduler().runTaskLater(plugin, new SendBlockChanges().init(updateBlocks, player), 1);
+
+}
+
+/**
+ Gets the numbers between two ints. This method works with positive & negative ints in any order.
+ * @param first One of the ints.
+ * @param second The other int.
+ * @return The numbers between the two ints.
+ */
+private List<Integer> getBetween(int first, int second) {
+  ArrayList<Integer> between = new ArrayList<>();
+
+  int diff = first - second;
+
+  if (diff > 0) {
+    for (int i = second; i < first; i++) {
+      between.add(i);
     }
+  }
 
-  }).start();
+  if (diff < 0) {
+    for (int i = second; i > first; i--) {
+      between.add(i);
+    }
+  }
+
+  return between;
+}
+
+/**
+ Gets the blocks surround the given location that won't be an outline & makes a copy of them, with their material type as LIGHT.
+ * @param getSurroundingOf The location to get the surrounding blocks of.
+ * @return The surrounding blocks with a material type of LIGHT.
+ */
+private Collection<BlockState> getNewCornerSurroundingStates(Location getSurroundingOf) {
+  Collection<BlockState> cornerStates = new ArrayList<>();
+
+  //makes the selected block glow.
+  for (Block block : getSurrounding(getSurroundingOf.getBlock(), Material.AIR)) {
+    //doesn't change a block to be light if it should be an outline.
+    if (selectionOutline.contains(block.getLocation())) continue;
+
+    BlockState blockState = block.getState();
+    blockState.setType(Material.LIGHT);
+    cornerStates.add(blockState);
+  }
+
+  return cornerStates;
 }
 }
+
